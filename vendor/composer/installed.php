941f32fb020981a7f086a572e2d373e44d277595
Gustavo Mussolino <gustavomussolino@hotmail.com>
2020-08-27 15:49:39 -0300

    setting composer and installing components

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '90d871ebee6559db06fac16d673012813c457aaf',
+    'name' => 'gumusso/coffeecode-data-layer',
+  ),
+  'versions' => 
+  array (
+    'coffeecode/datalayer' => 
+    array (
+      'pretty_version' => '1.1.7',
+      'version' => '1.1.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6a51fa227bba003be11519e7efa8338bc87460f5',
+    ),
+    'gumusso/coffeecode-data-layer' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '90d871ebee6559db06fac16d673012813c457aaf',
+    ),
+  ),
+);
